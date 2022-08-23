5e6a4de0728b9bdd4adb4040e793fc8675dec3ea
OrHaN_1982 <aliyevorhan82@gmail.com>
2022-08-23 23:36:30 +0400

    Add project files.

---
+++ Parse_Blog/HtmlTextCleaner.cs
@@ -0,0 +1,42 @@
+﻿using HtmlAgilityPack;
+using System.Text.RegularExpressions;
+
+namespace Parse_Blog
+{
+    public static class HtmlTextCleaner
+    {
+        /// <summary>
+        /// Clean html chracters whitespace and new line from text
+        /// </summary>
+        /// <param name="nodes">List of nodes</param>
+        /// <returns>Reverse list of all text</returns>
+        public static List<string> CleanBlogText(List<HtmlNode> nodes)
+        {
+            var cleanText = new List<string>();
+            foreach (var item in nodes)
+            {
+                var text = Regex.Replace(item.InnerText, @"( |\t|\r?\n)\1+", "$1").Replace("&nbsp;", "");
+                if (!String.IsNullOrEmpty(text))
+                {
+                    cleanText.Add(text);
+                }
+
+            }
+            cleanText.Reverse();
+            return cleanText;
+        }
+
+        /// <summary>
+        /// Clean html string
+        /// </summary>
+        /// <param name="str">inner text</param>
+        /// <returns>string</returns>
+        public static string CleanSingleLine(string text)
+        {
+            string cleanText = Regex.Replace(text, @"\t|\n|\r", "").Replace("&nbsp;", "").Trim();
+            cleanText = String.Join(' ', cleanText.Split(' ', StringSplitOptions.RemoveEmptyEntries));
+            return cleanText;
+        }
+
+    }
+}
---
+++ Parse_Blog/Program.cs
@@ -0,0 +1,48 @@
+﻿using HtmlAgilityPack;
+using System.Text.RegularExpressions;
+
+namespace Parse_Blog
+{
+    /// <summary>
+    /// Created: 23.08.22
+    /// Author:  Aliyev Orkhan
+    /// </summary>
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                HtmlWeb web = new HtmlWeb();
+
+                HtmlDocument doc = await web.LoadFromWebAsync("https://www.wakeupdata.com/blog/news-we-are-now-premium-google-css-partners");
+
+                var blogTitle = doc.DocumentNode.SelectNodes("//div/h1").First().InnerText ?? throw new ArgumentNullException();
+
+                //I'm not sure if it is needed on the assignment, but I also added
+                var postDateInfo = doc.DocumentNode.SelectNodes("//div[2]/p").First().InnerText ?? throw new ArgumentNullException();
+
+                var blogAllText = doc.DocumentNode.SelectNodes("//*[@id=\"hs_cos_wrapper_post_body\"]/p").ToList() ?? throw new ArgumentNullException();
+
+
+                // enables showing the euro symbol in the console
+                Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+                var result = HtmlTextCleaner.CleanBlogText(blogAllText);
+
+                result.ForEach(x => Console.WriteLine(x));
+                Console.WriteLine(HtmlTextCleaner.CleanSingleLine(postDateInfo));
+                Console.WriteLine(HtmlTextCleaner.CleanSingleLine(blogTitle));
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+        }
+
+
+    }
+}
